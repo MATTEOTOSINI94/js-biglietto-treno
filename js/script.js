fa3d0263f1724e664642c09ca25f7ac35fc2fbe4
MATTEOTOSINI94 <matteotosini300@icloud.com>
2021-10-08 17:36:36 +0200

    mod

--- js/script.js
+++ js/script.js
@@ -1,41 +1,34 @@
-const etaPasseggero = parseInt(prompt("Inserire la propria età"));
+const etaPasseggero = parseInt(prompt("Inserire la propria età"))
 console.log(etaPasseggero)
 
-const kmPasseggero =  prompt("Inserire il numero di km da percorrere")
+const kmPasseggero =  parseInt(prompt("Inserire il numero di Km da percorrere"))
 console.log(kmPasseggero)
 
 const kmPrice = 0.21
 console.log(kmPrice)
 
-const priceTot = parseFloat(kmPrice) * parseFloat(kmPasseggero)
-console.log (parseFloat(kmPrice) * parseFloat(kmPasseggero))
+const priceTot = kmPrice * kmPasseggero
+console.log("prezzo totale senza sconto: "+ priceTot.toFixed(2))
 
-const priceUnder = ((parseFloat(priceTot) * 20) / 100)
-
-
-
-
-
+const priceUnder = priceTot * 0.2
+let priceOver = priceTot * 0.4
 
 
 
 if (etaPasseggero < 18){
     alert("Hai diritto a uno sconto del 20%")
-    let priceTotUnder = (parseFloat(priceTot) - parseFloat(priceUnder))
-    let roundDbl = (Math.round(priceTotUnder*100.0)/100.0)
-    alert("complimenti questo è il suo prezzo scontato del 20% " + roundDbl)
+    let priceTotUnder = priceTot - priceUnder
+    alert("complimenti questo è il suo prezzo scontato del 20% " + priceTotUnder.toFixed(2)+"£")
    
 }
 
 else if (etaPasseggero >= 65){
     alert("hai diritto a uno sconto del 40%")
-    let priceTotOver = (parseFloat(priceTot) - parseFloat(over))
-    let roundDb2 = (Math.round(priceTotOver*100.0)/100.0)
-    alert("complimenti questo è il suo prezzo scontato del 40% " + roundDb2)   
-
+    let priceTotOver = priceTot - priceOver 
+    alert("complimenti questo è il suo prezzo scontato del 40% " + priceTotOver.toFixed(2)+"£")   
 }
-else (etaPasseggero >= 18) {
 
-    alert("Complimenti prenotazione effettuata prezzo: " + priceTot)
+else {
+    alert("Complimenti prenotazione effettuata prezzo: " + priceTot.toFixed(2)+"£")
 }
 
