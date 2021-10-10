05927f853ff730a6500fa0ce6626f17397329375
MATTEOTOSINI94 <matteotosini300@icloud.com>
2021-10-10 18:23:17 +0200

    mod

--- js/script.js
+++ js/script.js
@@ -1,25 +1,28 @@
 const etaPasseggero = parseInt(prompt("Inserire la propria età"))
-console.log(etaPasseggero)
 
 const kmPasseggero =  parseInt(prompt("Inserire il numero di Km da percorrere"))
-console.log(kmPasseggero)
 
 const kmPrice = 0.21
-console.log(kmPrice)
 
 const priceTot = kmPrice * kmPasseggero
 console.log("prezzo totale senza sconto: "+ priceTot.toFixed(2))
 
 const priceUnder = priceTot * 0.2
+console.log("prezzo scontato del 20% "+ priceUnder)
 let priceOver = priceTot * 0.4
+console.log("prezzo scontato del 40% " + priceUnder)
 
 
 
-if (etaPasseggero < 18){
+
+if (isNaN(etaPasseggero && kmPasseggero)){
+    alert("Non hai inserito numeri riprova correttamente")
+}
+
+else if (etaPasseggero < 18){
     alert("Hai diritto a uno sconto del 20%")
     let priceTotUnder = priceTot - priceUnder
     alert("complimenti questo è il suo prezzo scontato del 20% " + priceTotUnder.toFixed(2)+"£")
-   
 }
 
 else if (etaPasseggero >= 65){
@@ -28,7 +31,7 @@ else if (etaPasseggero >= 65){
     alert("complimenti questo è il suo prezzo scontato del 40% " + priceTotOver.toFixed(2)+"£")   
 }
 
-else {
+else  {
     alert("Complimenti prenotazione effettuata prezzo: " + priceTot.toFixed(2)+"£")
 }
 
